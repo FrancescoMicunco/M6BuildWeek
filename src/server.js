5ab26d551f80f56edc3e198e1ddc3aa2e66df448
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-20 12:06:34 +0100

    structure

--- src/server.js
+++ src/server.js
@@ -11,4 +11,20 @@ server.use(express.json())
 server.use(cors());
 
 
-server.listen(port, () => { console.log("the server is running") })
+//========= connection area =====================
+//===============================================
+
+Planner.hasMany(Tasks, { onDelete: "CASCADE" });
+Tasks.belongsTo(Planners, { onDelete: "CASCADE" });
+
+
+//==========================================
+
+console.table(server);
+server.listen(process.env.PORT || 3001, async() => {
+    console.log(`Server is running`);
+    await testDB();
+    await sequelize.sync({ logging: true });
+});
+
+server.on("error", (error) => console.log("Server is not running", error));
