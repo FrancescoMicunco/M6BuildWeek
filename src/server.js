d1a5793ebfe83c01b29df1aa4be925e7fc481799
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-12-20 12:39:54 +0100

    structure and connection to database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-
+import { testDB } from './modules/connect.js'
+import sequelize from './modules/connect.js'
 
 const server = express()
 
@@ -13,14 +14,14 @@ server.use(cors());
 // ==========  end points ===============
 //=======================================
 
-server.use("/tasks", tasksRouter);
-server.use("/planners", plannersRouter);
+// server.use("/tasks", tasksRouter);
+// server.use("/planners", plannersRouter);
 
 //========= connection area =====================
 //===============================================
 
-Planner.hasMany(Tasks, { onDelete: "CASCADE" });
-Tasks.belongsTo(Planners, { onDelete: "CASCADE" });
+// Planner.hasMany(Tasks, { onDelete: "CASCADE" });
+// Tasks.belongsTo(Planners, { onDelete: "CASCADE" });
 
 
 //==========================================
@@ -29,7 +30,7 @@ console.table(server);
 server.listen(process.env.PORT || 3001, async() => {
     console.log(`Server is running`);
     await testDB();
-    await sequelize.sync({ logging: true });
+    await sequelize.sync({ logging: true, alter: true });
 });
 
 server.on("error", (error) => console.log("Server is not running", error));
