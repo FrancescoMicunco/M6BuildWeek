b8d3fd7be41049856215863ffb8e36ea5666eebe
aizada869-nazarova <83946225+aizada869-nazarova@users.noreply.github.com>
2021-12-20 11:54:18 +0000

    add

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 
+const server =express()
 
-const server = express()
 
-const port = process.env.PORT
 
 
 server.use(express.json())
@@ -13,23 +12,23 @@ server.use(cors());
 // ==========  end points ===============
 //=======================================
 
-server.use("/tasks", tasksRouter);
-server.use("/planners", plannersRouter);
+// server.use("/tasks", tasksRouter);
+// server.use("/planners", plannersRouter);
 
 //========= connection area =====================
 //===============================================
 
-Planner.hasMany(Tasks, { onDelete: "CASCADE" });
-Tasks.belongsTo(Planners, { onDelete: "CASCADE" });
+// Planner.hasMany(Tasks, { onDelete: "CASCADE" });
+// Tasks.belongsTo(Planners, { onDelete: "CASCADE" });
 
 
 //==========================================
 
-console.table(server);
+
 server.listen(process.env.PORT || 3001, async() => {
     console.log(`Server is running`);
-    await testDB();
-    await sequelize.sync({ logging: true });
+    // await testDB();
+    // await sequelize.sync({ logging: true });
 });
 
 server.on("error", (error) => console.log("Server is not running", error));
